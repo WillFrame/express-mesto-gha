135b3b0fe949151d9d56732a88d544753ddeeb36
WillFrame <32850788+WillFrame@users.noreply.github.com>
2022-06-23 18:37:29 +0300

    Add files via upload

--- models/card.js
+++ models/card.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const cardSchema = mongoose.Schema({
   name: {
@@ -10,6 +11,10 @@ const cardSchema = mongoose.Schema({
   link: {
     type: String,
     required: true,
+    validate: {
+      validator(url) { return validator.isURL(url); },
+      message: 'Ошибка в url',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
